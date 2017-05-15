cd2f9fcfbb02a5a7b7efbf746f9ced6e698c6b2d
Sergej Frank <git@sergej-frank.de>
2017-05-15 10:52:17 +0200

    added some more struct

---
+++ src/main/java/de/itech/netcalc/Main.java
@@ -0,0 +1,10 @@
+package de.itech.netcalc;
+
+/**
+ * Created by sergej on 15.05.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
