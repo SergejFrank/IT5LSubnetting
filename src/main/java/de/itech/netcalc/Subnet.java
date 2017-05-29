25f7c0f66a1c793d1b0da59188b7b84dc59af47a
Valentin <valentin.beller@gmail.com>
2017-05-29 13:00:57 +0200

    added IPv6 to Host, Subnet and Network

--- src/main/java/de/itech/netcalc/Subnet.java
+++ src/main/java/de/itech/netcalc/Subnet.java
@@ -1,19 +1,42 @@
 package de.itech.netcalc;
 
-public class Subnet extends NetworkBase {
+public class Subnet extends NetworkBase{
+    private IPv6Address networkIdV6;
+    private int prefixV6;
     private Host[] hosts;
 
-    public Subnet(IPv4Address address, IPv4Address mask){
-        this.setAddress(new IPv4Address(address.getValue() & mask.getValue()));
-        this.setMask(mask);
+    public Subnet(IPv4Address netwotkIdV4, IPv4Address networkMaskV4) throws Exception {
+        this.setAddress(new IPv4Address(netwotkIdV4.getValue() & networkMaskV4.getValue()));
+        this.setMask(networkMaskV4);
         hosts = new Host[getMaxHosts()];
         for(int i = 1; i <= getMaxHosts(); i++) {
-            Host host = new Host(new IPv4Address(getAddress().getValue() + i), null);
+            Host host = new Host(this, new IPv4Address(getAddress().getValue() + i), null, null);
+            hosts[i-1] = host;
+        }
+    }
+
+    public Subnet(IPv4Address networkIdV4, IPv4Address networkMaskV4, IPv6Address networkIdV6, int prefixV6) throws Exception {
+        if(prefixV6 < 0 || prefixV6 > 128) throw new IllegalArgumentException(prefixV6 + " is not a valid IPv6 Prefix");
+        this.setAddress(new IPv4Address(networkIdV4.getValue() & networkMaskV4.getValue()));
+        this.setMask(networkMaskV4);
+        this.networkIdV6 = networkIdV6;
+        this.prefixV6 = prefixV6;
+        hosts = new Host[getMaxHosts()];
+        for(int i = 1; i <= getMaxHosts(); i++) {
+            Host host = new Host(this, new IPv4Address(getAddress().getValue() + i), null, null);
             hosts[i-1] = host;
         }
     }
 
     public Host[] getHosts(){
         return hosts;
+    }
+
+    public IPv6Address getNetworkIdV6() {
+        return networkIdV6;
+    }
+
+    public int getPrefixV6() {
+        return prefixV6;
     }
 }
