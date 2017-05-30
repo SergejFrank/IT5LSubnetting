df1d1fac538d165c7bf452cd6645ed8da810502d
Marcel <marcel.schmidt89@web.de>
2017-05-30 11:48:17 +0200

    Networks Tab update

--- src/main/java/de/itech/netcalc/NetworksPanel.java
+++ src/main/java/de/itech/netcalc/NetworksPanel.java
@@ -1,34 +1,69 @@
 package de.itech.netcalc;
 
 import javax.swing.*;
+import javax.swing.border.EtchedBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * Created by Marcel on 29.05.2017.
  */
-public class NetworksPanel extends JPanel {
+public class NetworksPanel extends JPanel{
 
+    private JLabel networksLabel;
+    private JLabel ipLabel;
+    private JTextField ipText;
+
+    // Sub panels
     private JPanel bottomPanel;
+    private JPanel inputPanel;
+    private JPanel buttonPanel;
+
+    // JList
     private JList netList;
     private DefaultListModel listModel;
-    private JLabel networksLabel;
 
+    // Buttons
     private JButton bTest1;
     private JButton bTest2;
     private JButton bTest3;
 
+    // Border
+
     public NetworksPanel(LayoutManager layoutManager, Network network){
         super(layoutManager);
 
-        bottomPanel = new JPanel(new FlowLayout());
         networksLabel = new JLabel("Networks");
-        listModel = new DefaultListModel();
-        netList = new JList(listModel);
+        ipLabel = new JLabel("IP: ");
+        ipText = new JTextField();
+        ipText.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
 
         // Buttons
-        bTest1 = new JButton("Test1");
+        bTest1 = new JButton("New");
         bTest2 = new JButton("Test2");
         bTest3 = new JButton("Test3");
+
+        // Panels
+        bottomPanel = new JPanel(new GridLayout(2,1));
+        inputPanel = new JPanel(new BorderLayout(42,5));
+        buttonPanel = new JPanel();
+
+
+        // JList
+        listModel = new DefaultListModel();
+        netList = new JList(listModel);
+        netList.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+
+
+        bTest1.addActionListener(new ButtonListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("TEST");
+            }
+        });
+        bTest2.addActionListener(new ButtonListener());
+        bTest3.addActionListener(new ButtonListener());
 
         // Configure networks list
         listModel.addElement(network);
@@ -44,13 +79,30 @@ public class NetworksPanel extends JPanel {
         JScrollPane listScrollPane = new JScrollPane(netList);
 
 
-        // Configure Networks Tab
+        // Configure Networks Layout
         networksLabel.setVerticalAlignment(JLabel.TOP);
         add(networksLabel, BorderLayout.LINE_START);
         add(netList);
-        bottomPanel.add(bTest1);
-        bottomPanel.add(bTest2);
-        bottomPanel.add(bTest3);
-        add(bottomPanel, BorderLayout.PAGE_END);    }
+        add(bottomPanel, BorderLayout.PAGE_END);
+
+        bottomPanel.add(inputPanel);
+        bottomPanel.add(buttonPanel);
+
+        ipLabel.setVerticalAlignment(JLabel.TOP);
+        inputPanel.add(ipLabel, BorderLayout.LINE_START);
+        inputPanel.add(ipText, BorderLayout.CENTER);
+
+        buttonPanel.add(bTest1);
+        buttonPanel.add(bTest2);
+        buttonPanel.add(bTest3);
+
+    }
+
+    public class ButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            System.out.println("Good job you pressed a button, what a man!");
+        }
+    }
 
 }
--- src/main/java/de/itech/netcalc/SubnetCalculatorFrame.java
+++ src/main/java/de/itech/netcalc/SubnetCalculatorFrame.java
@@ -17,26 +17,17 @@ public class SubnetCalculatorFrame extends JFrame {
     // Panels
     private NetworksPanel netsPan;
 
-    private JPanel networksPanel = new JPanel(new BorderLayout(5, 5));
-    private JPanel networksBottomPanel = new JPanel();
-    private JPanel subnetsPanel = new JPanel();
+    private JPanel subnetsPanel = new SubnetPanel();
     private JPanel hostsPanel = new JPanel();
     private JLabel networksLabel = new JLabel("Networks");
     private JLabel subnetsLabel = new JLabel("Subnets");
     private JLabel hostsLabel = new JLabel("Hosts");
-    private JTabbedPane tabbedPane = new JTabbedPane();
-    // Network List
-    private JList list;
-    private DefaultListModel listModel;
-
-    // Buttons
-    JButton test1 = new JButton("Test1");
-    JButton test2 = new JButton("Test2");
-    JButton test3 = new JButton("Test3");
 
     public SubnetCalculatorFrame(String title, Network network){
         super(title);
         this.network = network;
+
+        netsPan = new NetworksPanel(new BorderLayout(5, 5), network);
 
         // Add Panels to the tabs
         tabbedPane.add("Networks", netsPan);
@@ -45,7 +36,7 @@ public class SubnetCalculatorFrame extends JFrame {
 
         add(tabbedPane);
 
-        //this.setSize(800, 600);
+        //setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
 
