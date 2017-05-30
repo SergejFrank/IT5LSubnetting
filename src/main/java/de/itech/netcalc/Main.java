5ae9771c5c522597d2f019e2b7c0742ba460ad74
Valentin <valentin.beller@gmail.com>
2017-05-30 22:57:32 +0200

    cleaned up main

--- src/main/java/de/itech/netcalc/Main.java
+++ src/main/java/de/itech/netcalc/Main.java
@@ -6,17 +6,6 @@ import de.itech.netcalc.net.Network;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Network baseNet = new Network(new IPv4Address(10,5,1,0), new IPv4Address(255,255,255,0));
-        baseNet.addSubnet(40);
-        baseNet.addSubnet(14);
-        baseNet.addSubnet(14);
-        baseNet.addSubnet(14);
-        baseNet.addSubnet(40);
-        baseNet.addSubnet(3);
-        baseNet.addSubnet(3);
-        baseNet.addSubnet(40);
-
-        // GUI
-        SubnetCalculatorFrame calcFrame = new SubnetCalculatorFrame("SubnetCalculator");
+        new SubnetCalculatorFrame("SubnetCalculator");
     }
 }
