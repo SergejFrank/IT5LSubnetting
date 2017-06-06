957233c0c5ae5a1abfa8cc526c3abb9bd20d4775
Valentin <valentin.beller@gmail.com>
2017-06-06 14:12:04 +0200

    added confirmation dialog helper method

--- src/main/java/de/itech/netcalc/gui/GuiUtils.java
+++ src/main/java/de/itech/netcalc/gui/GuiUtils.java
@@ -4,6 +4,8 @@ import de.itech.netcalc.net.IPv4Address;
 import de.itech.netcalc.net.NetUtils;
 import de.itech.netcalc.net.Network;
 
+import javax.swing.*;
+
 class GuiUtils {
     static String getInitialSubnetString(Network network) {
         IPv4Address networkId = network.getNetworkIdV4();
@@ -15,5 +17,11 @@ class GuiUtils {
         if(prefix >= 8)
             return networkId.getOct1() + ".";
         return "";
+    }
+
+    static boolean confirmation(String title, String message) {
+        int input = JOptionPane.showConfirmDialog(null, message, title,
+                JOptionPane.OK_CANCEL_OPTION);
+        return input == JOptionPane.OK_OPTION;
     }
 }
