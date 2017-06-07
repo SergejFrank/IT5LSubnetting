27430727f2c32f3052f8e42c445e50b5da4a0a6c
Valentin <valentin.beller@gmail.com>
2017-06-07 10:17:55 +0200

    added docs to Main

--- src/main/java/de/itech/netcalc/Main.java
+++ src/main/java/de/itech/netcalc/Main.java
@@ -2,7 +2,14 @@ package de.itech.netcalc;
 
 import de.itech.netcalc.gui.SubnetCalculatorFrame;
 
+/**
+ * The Main class provides the main() method as the entry point.
+ */
 public class Main {
+    /**
+     * Creates a new SubnetCalculatorFrame and shows it.
+     * @param args ignored
+     */
     public static void main(String[] args) {
         new SubnetCalculatorFrame("SubnetCalculator");
     }
