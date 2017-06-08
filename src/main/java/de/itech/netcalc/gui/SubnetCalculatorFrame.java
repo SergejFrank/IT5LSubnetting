2393601c643c04356317c962f0fa6547c4872fbc
Valentin <valentin.beller@gmail.com>
2017-06-08 11:29:18 +0200

    added Menu scaffold

--- src/main/java/de/itech/netcalc/gui/SubnetCalculatorFrame.java
+++ src/main/java/de/itech/netcalc/gui/SubnetCalculatorFrame.java
@@ -2,10 +2,13 @@ package de.itech.netcalc.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class SubnetCalculatorFrame extends JFrame {
     public SubnetCalculatorFrame(String title){
         super(title);
+
+        initialiteMenu();
 
         add(new TreePanel());
 
@@ -16,5 +19,47 @@ public class SubnetCalculatorFrame extends JFrame {
         setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2); // screen center
 
         setVisible(true);
+    }
+
+    private void initialiteMenu() {
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("Datei");
+        fileMenu.add(new AbstractAction("Neu") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        fileMenu.addSeparator();
+        fileMenu.add(new AbstractAction("Öffnen") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        fileMenu.add(new AbstractAction("Speichern unter...") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        fileMenu.addSeparator();
+        fileMenu.add(new AbstractAction("Beenden") {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        JMenu viewMenu = new JMenu("Ansicht");
+        JMenu notationMenu =  new JMenu("Schreibweise");
+        notationMenu.add(new JRadioButtonMenuItem("Dezimal"));
+        notationMenu.add(new JRadioButtonMenuItem("Binär"));
+        viewMenu.add(notationMenu);
+
+        menuBar.add(fileMenu);
+        menuBar.add(viewMenu);
+        setJMenuBar(menuBar);
     }
 }
