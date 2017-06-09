27b3d781834db975f74d1b5e289cbd0d68d61e74
Valentin <valentin.beller@gmail.com>
2017-06-09 11:08:35 +0200

    added SystemLookAndFeel

--- src/main/java/de/itech/netcalc/Main.java
+++ src/main/java/de/itech/netcalc/Main.java
@@ -2,6 +2,8 @@ package de.itech.netcalc;
 
 import de.itech.netcalc.gui.SubnetCalculatorFrame;
 
+import javax.swing.*;
+
 /**
  * The Main class provides the main() method as the entry point.
  */
@@ -10,7 +12,8 @@ class Main {
      * Creates a new SubnetCalculatorFrame and shows it.
      * @param args ignored
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         new SubnetCalculatorFrame("SubnetCalculator");
     }
 }
